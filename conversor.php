815da80a33a7c82e400767de54d3c7ca5a46f635
Fernando Antônio <145825036+DevFernando06@users.noreply.github.com>
2025-03-16 02:31:10 -0300

    update

--- conversor.php
+++ conversor.php
@@ -12,13 +12,18 @@
       <h1>Valor Convertido</h1>
     </header>
     <?php 
+
     //DATA client
+
     $dateStart = date("m-d-Y", strtotime("-7 days"));
     $dateEnd = date("m-d-Y");
+
     // API Banco central do Brasil
+
     $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$dateStart.'\'&@dataFinalCotacao=\''.$dateEnd.'\'&$top=1&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
     //Decodificando json e fazendo o request 
+    
     $data = json_decode(file_get_contents($url), true);
 
     //Conversão e coleta de dados
